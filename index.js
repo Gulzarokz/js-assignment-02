dde976aa56037ccc3b45c61b6fa58d323e02e38c
Gulzarokz <gokz22301@gamil.com>
2024-10-08 16:42:20 +0500

    Done Maths expersion and Unfimilar operators increment/decrements

---
+++ index.js
@@ -0,0 +1,98 @@
+
+// Chapter No 5 maths expersion
+// const firstNumber = 2;
+// const secondNumber = 5;
+// const result = firstNumber + secondNumber;
+// console.log("Total number is = " +result);
+
+
+// concatenation
+// const result = 2 + "12";
+// console.log(result);
+
+
+// const resultt = 2 + "23" + 2;
+// console.log(resultt);
+
+// const addition = 2 + 2 + "6";
+// console.log(addition);
+
+
+// const result = 2 + 2 + "5" * 9;
+// console.log(result);
+ 
+
+// const firstNumber = 9;
+// const secondNumber = 8;
+// const result = firstNumber - secondNumber;
+// console.log(result);
+
+
+// const firstNumber = 9;
+// const secondNumber = 8;
+// const result = firstNumber * secondNumber;
+// console.log(result);
+
+// const result = "2" * 4;
+// console.log(result);
+
+// const firstNumber = 8;
+// const secondNumber = 4;
+// const result = firstNumber / secondNumber;
+// console.log(result);
+
+
+// const firstNumber = 17;
+// const secondNumber = 4;
+// const result = firstNumber + secondNumber;
+// console.log(`The result of ${firstNumber} & ${secondNumber} is =  ${result}`);
+
+
+
+// Chapter No 6 Maths Experssion with Unfimiliar Operator
+
+// Increment Operaotor ++
+
+// let salary = 200;
+// console.log(++salary);
+
+// pre increment ++salary, 
+// let salary = 159;
+// const incrementSalary = ++salary;
+// console.log(incrementSalary);
+
+
+
+
+// post increment salary++
+
+// let salary = 159;
+// const incrementSalary = salary++;
+// console.log(incrementSalary);
+// console.log(salary);
+
+// pre decrement ++salary,
+
+// let charging = 74;
+// console.log(charging--);
+// console.log(charging);
+
+// post decrement salary++,
+
+// let charging = 74;
+// const result = (--charging);
+// // console.log(--charging);
+// console.log(result);
+
+//  var num = 1;
+//  var newNum = num++;
+// console.log(newNum);
+// console.log(num);
+
+ var num = 1;
+ var newNum = ++num;
+ console.log(newNum);
+//  console.log(num);
+ 
+ 
+
